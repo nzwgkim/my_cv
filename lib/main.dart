b649ce5cf5845d8b11c49ce530daae391b412958
Woody <nz.wgkim@gmail.com>
2022-12-29 18:20:44 +1300

    Dialpad

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'app/locations.dart' as locations;
+import 'package:my_cv/app/Dialpad.dart';
+// import 'package:my_cv/app/myCvTable.dart';
+// import 'package:my_cv/app/map_sample.dart';
+// import 'package:my_cv/app/myCvTabPage.dart';
+// import 'app/myCvGridTable.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final Map<String, Marker> _markers = {};
-  Future<void> _onMapCreated(GoogleMapController controller) async {
-    final googleOffices = await locations.getGoogleOffices();
-    setState(() {
-      _markers.clear();
-      for (final office in googleOffices.offices) {
-        final marker = Marker(
-          markerId: MarkerId(office.name),
-          position: LatLng(office.lat, office.lng),
-          infoWindow: InfoWindow(
-            title: office.name,
-            snippet: office.address,
-          ),
-        );
-        _markers[office.name] = marker;
-      }
-    });
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const String _title = "Woody's CV";
+
     return MaterialApp(
+      title: _title,
+      color: Colors.red,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Google Office Locations'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: const CameraPosition(
-            target: LatLng(0, 0),
-            zoom: 2,
-          ),
-          markers: _markers.values.toSet(),
-        ),
+        backgroundColor: Colors.yellow,
+        appBar: AppBar(title: const Text(_title)),
+        body: dialPad(),
       ),
     );
   }
